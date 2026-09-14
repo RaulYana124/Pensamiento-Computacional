50c9e74a7cd1ab7122b82f2a6ac87b221bcdb64e
RaulYana124 <rmachacayana@gmail.com>
2026-09-14 01:34:28 -0500

    Add Ejercicio3.cpp with basic arithmetic operations

---
+++ Ejercicio3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    a += 3;
+    b -= a;
+
+    cout << "A = " << a << endl;
+    cout << "B = " << b << endl;
+
+    return 0;
+}
